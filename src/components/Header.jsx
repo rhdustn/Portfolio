b2716ed0b1794dd6b7577262e1850d0357a804b4
yeonsu <gys2116@gmail.com>
2023-12-08 10:23:29 +0900

    수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+// Header.js file
+
+import React from 'react';
+import logo from "../img/logo.png"
 
 const Header = () => {
-    return (
-        <div className="w-full bg-white ">
-            <div>Header의 이미지</div>
-            <div>
-                <ul className>
-                    <li>프로젝트</li>
-                    <li>기술스택</li>
-                </ul>
-            </div>
+  return (
+    <header className='bg-white shadow fixed w-full z-10'>
+      <div className="container mx-auto flex items-center justify-between py-4">
+        <div>
+          <img src={logo} alt="Logo" className="h-8" />
         </div>
-    )
-}
+        <nav className="flex items-center space-x-4">
+          <div className="text-gray-800 hover:text-gray-600">About</div>
+          <div className="text-gray-800 hover:text-gray-600">Projects</div>
+          <div className="text-gray-800 hover:text-gray-600">Contact</div>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
